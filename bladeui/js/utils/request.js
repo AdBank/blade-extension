7302ae660c78c650abedf2559f37f9b521580f7b
Maxym Dyachenko <maksym.dyachenko@axondevgroup.com>
2019-01-15 18:01:29 +0200

    NEW: SLID-1478 - add request function

--- bladeui/js/utils/request.js
+++ bladeui/js/utils/request.js
@@ -2,26 +2,45 @@
 
 const URL = "http://ec2-3-81-128-247.compute-1.amazonaws.com:8088";
 
-const request = {
-  post: (url, data) => post(data, url),
-  get: (url) => get(url)
-};
-
-function post(data = {}, url)
+function makeRequest(opts)
 {
-  return fetch(URL + url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json());
+  return new Promise(((resolve, reject) =>
+  {
+    const xhr = new XMLHttpRequest();
+    xhr.open(opts.method, URL + opts.url);
+    xhr.onload = function()
+    {
+      if (this.status >= 200 && this.status < 300)
+      {
+        resolve(this);
+      }
+      else
+      {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+          error: JSON.parse(this.response).error
+        });
+      }
+    };
+    xhr.onerror = function()
+    {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText,
+        error: JSON.parse(this.response).error
+      });
+    };
+    if (opts.headers)
+    {
+      Object.keys(opts.headers).forEach((key) =>
+      {
+        xhr.setRequestHeader(key, opts.headers[key]);
+      });
+    }
+
+    xhr.send(JSON.stringify(opts.data));
+  }));
 }
 
-function get(url)
-{
-  return fetch(URL + url).then(response => response.json());
-}
-
-module.exports = request;
+module.exports = makeRequest;
