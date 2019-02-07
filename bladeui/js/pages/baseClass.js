0ddde96d45cd32ce3401ca3f5eb53754b76a742b
Maxym Dyachenko <maksym.dyachenko@axondevgroup.com>
2019-02-07 17:31:39 +0200

    NEW: AD-1575 - switch on off extension
    # Conflicts:
    #	blade.idle.js
    #	bladeui/js/pages/baseClass.js
    #	lib/contentFiltering.js

--- bladeui/js/pages/baseClass.js
+++ bladeui/js/pages/baseClass.js
@@ -2,6 +2,7 @@
 
 /* eslint-disable max-len, no-console */
 
+const {isPageWhitelisted} = require("../popup.utils.js");
 class BaseClass
 {
   constructor({onChangeView})
@@ -27,6 +28,42 @@ class BaseClass
     closeButton && closeButton.addEventListener("click", this.handleClose.bind(this));
     settingsTabs && settingsTabs.addEventListener("click", this.handleSettingsTabClick.bind(this));
     this.menuList && this.menuList.addEventListener("click", this.handleGoToMenuView.bind(this));
+
+    browser.tabs.query({active: true, lastFocusedWindow: true}, tabs =>
+    {
+      this.initToggleOnOff({id: tabs[0].id, url: tabs[0].url});
+    });
+  }
+
+  initToggleOnOff(tab)
+  {
+    this.toggler = document.getElementById("checkbox");
+
+    isPageWhitelisted(tab, whitelisted =>
+    {
+      if (whitelisted)
+      {
+        this.toggler.checked = false;
+      }
+    });
+
+    this.toggler.addEventListener("change", () =>
+    {
+      if (this.toggler.checked)
+      {
+        browser.runtime.sendMessage({
+          type: "filters.unwhitelist",
+          tab
+        });
+      }
+      else
+      {
+        browser.runtime.sendMessage({
+          type: "filters.whitelist",
+          tab
+        });
+      }
+    });
   }
 
   handleGoToMenuView(e)
