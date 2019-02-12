7379221b6bce11ebd6d046f8da5c41b728a6ca25
Maxym Dyachenko <maksym.dyachenko@axondevgroup.com>
2019-02-12 15:29:53 +0200

    NEW: AD-1576 - create manual transfer skeleton

--- bladeui/js/html/manualTransfer.js
+++ bladeui/js/html/manualTransfer.js
@@ -1,14 +1,20 @@
 "use strict";
 
+/* eslint-disable max-len */
+
 const headerNavbar = require("./common/headerNavbar");
+const header = require("./common/header");
 const menuList = require("./common/menuList");
 
 const html = `
 ${menuList("transfers")}
-<div class="manual-transfers-view">
+<div class="manual-transfers-view flex-column">
   ${headerNavbar("transfers")}
   <div class="content">
-    it is transfers
+    <p class="manual-transfer-form-description">
+      <i class="fa fa-long-arrow-left" id="back-button"></i>
+      Make a Manual Transfer
+    </p>
   </div>
 </div>
 `;
