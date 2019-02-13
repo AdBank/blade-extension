29ee4c0e307eb4a05d3f4547f860a3e7499848ec
Ira Novykova <inovykova@axondevgroup.com>
2019-02-13 17:37:03 +0200

    FIX: AD-1565 - after failed sending referral change btn content "SEND", more space for error display

--- bladeui/js/pages/referralsForm.js
+++ bladeui/js/pages/referralsForm.js
@@ -69,6 +69,7 @@ class ReferralsForm extends BaseClass
 
   highlightErrors(email, message)
   {
+    this.sendButton.innerHTML = "SEND";
     if (!this.validateEmail(email))
     {
       this.email.classList.add("input-invalid");
@@ -104,7 +105,7 @@ class ReferralsForm extends BaseClass
       })
       .catch((err) =>
       {
-        this.sendButton.innerHTML = loader(false);
+        this.sendButton.innerHTML = "SEND";
         this.sendButton.disabled = false;
         this.emailError.innerHTML = err.error;
       });
